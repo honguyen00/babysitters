0679295a7e48aa563355ee82834f1925bb043bd7
Simon Hamblin <simon.hamblin@gmail.com>
2023-11-26 16:11:57 +1100

    navbar partial added

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const session = require('express-session');
+
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// handlebars dependencies
+const exphbs = require('express-handlebars');
+const path = require('path');
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,6 +32,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+const userRoutes = require('./controllers/api/userRoutes');
+const groupRoutes = require('./controllers/api/groupRoutes');
+const postRoutes = require('./controllers/api/postRoutes');
+
+// Set Handlebars as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.render('home', { layout: 'main', title: 'Home' });
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
