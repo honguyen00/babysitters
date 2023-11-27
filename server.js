f2abb9b245962c0d18f4fb6a32577a6195d27fec
honguyen00 <lehonguyen00@gmail.com>
2023-11-27 11:55:54 +1100

    move routing in server.js to controllers file, rename home.handlebars to homepage.handlebars

--- server.js
+++ server.js
@@ -32,18 +32,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-const userRoutes = require('./controllers/api/userRoutes');
-const groupRoutes = require('./controllers/api/groupRoutes');
-const postRoutes = require('./controllers/api/postRoutes');
 
 // Set Handlebars as the default template engine.
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => {
-    res.render('home', { layout: 'main', title: 'Home' });
-});
+// app.get('/', (req, res) => {
+//     res.render('home', { layout: 'main', title: 'Home' });
+// });
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
