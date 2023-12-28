6d1879c2828357073e1b6cfb88f3f1b65b45f7d5
honguyen00 <lehonguyen00@gmail.com>
2023-12-28 17:45:39 +1100

    changing the font of body and headings, fixing login error message and change the width and height of profile picture

--- public/js/errorHandler.js
+++ public/js/errorHandler.js
@@ -1,49 +1,54 @@
+let isAnimating = false;
 function errorMessage(form, errorP, message)  {
-    form.find(`#${errorP}`)[0].innerText = message;
     const xMax = 5;
     const yMax = 5;
-    anime({
-    targets: `#${errorP}`,
-    easing: 'easeInOutSine',
-    duration: 500,
-    translateX: [
-        {
-        value: xMax * -1,
-        },
-        {
-        value: xMax,
-        },
-        {
-        value: xMax/-2,
-        },
-        {
-        value: xMax/2,
-        },
-        {
-        value: 0
-        }
-    ],
-    translateY: [
-        {
-        value: yMax,
-        },
-        {
-        value: yMax  * -1,
-        },
-        {
-        value: yMax/2,
-        },
-        {
-        value: yMax/-2,
-        },
-        {
-        value: 0
-        }
-    ],
-    });
-    setTimeout(() => {
-        form.find(`#${errorP}`)[0].innerText = "";
-    }, 1500);
+    if(!isAnimating) {
+        isAnimating = true;
+        form.find(`#${errorP}`)[0].innerText = message;
+        anime({
+            targets: `#${errorP}`,
+            easing: 'easeInOutSine',
+            duration: 500,
+            translateX: [
+                {
+                value: xMax * -1,
+                },
+                {
+                value: xMax,
+                },
+                {
+                value: xMax/-2,
+                },
+                {
+                value: xMax/2,
+                },
+                {
+                value: 0
+                }
+            ],
+            translateY: [
+                {
+                value: yMax,
+                },
+                {
+                value: yMax  * -1,
+                },
+                {
+                value: yMax/2,
+                },
+                {
+                value: yMax/-2,
+                },
+                {
+                value: 0
+                }
+            ],
+            });
+        setTimeout(() => {
+            form.find(`#${errorP}`)[0].innerText = "";
+            isAnimating = false;
+        }, 1500);
+    }
 }
 
 export default errorMessage;
